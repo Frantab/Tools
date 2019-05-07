d64bda806cdecf28509352a7df92545535078cca
frantisek.bachrony <frantisek.bachrony@firma.seznam.cz>
2019-05-07 13:05:21 +0200

    Javascript: first version of Tools.

---
+++ src/Tools.js
@@ -0,0 +1,149 @@
+/**
+ * This function appends one or more HTML elements (childs) to HTML element (parent).
+ * @param {HTMLElement} parent - HTML parent element
+ * @param {array<HTMLElement>} childs - Array of HTML elements
+ * @example
+ * const parent = document.body;
+ * const child1 = document.body.querySelector('.class1');
+ * const child2 = document.body.querySelector('.class2');
+ *
+ * appendChilds(parent, child1, child2);
+ */
+export const appendChilds = (parent, ...childs) => {
+	childs.forEach(child => {
+		if (child && parent) {
+			parent.appendChild(child);
+		}
+	});
+};
+
+/**
+ * This function create HTML element.
+ * @param {string} type - Type of HTML element (div, p, a, ...)
+ * @param {object} specifications - Object which contains attributes for HTML element (class, name, type, ...)
+ * @return {HTMLElement}
+ * @example
+ * const newElement = createWholeElement('div', {
+ *     'class': ['class1', 'class2', 'class3'],
+ *     'data-id': 'div0001'
+ * });
+ */
+export const createWholeElement = (type, specifications = {}) => {
+	/** @type {HTMLElement} */
+	const newElement = document.createElement(type);
+
+	for (const attribute in specifications) {
+		if (attribute === 'innerHTML' || attribute === 'innerText') {
+			newElement[attribute] = specifications[attribute];
+		} else if (attribute === 'class') {
+			newElement.className = specifications[attribute].reduce((className, cls, index) => {
+				return className += index === specifications[attribute].length - 1 ? cls : `${cls} `;
+			}, '');
+		} else {
+			newElement.setAttribute(attribute, specifications[attribute]);
+		}
+	}
+	return newElement;
+};
+
+/**
+ * This function add listener for event on the HTML element or elements.
+ * @param {string} event - Type of event
+ * @param {function} fn - Method which handle event (callback)
+ * @param {array<HTMLElement>} elements - Array of HTML elements
+ * @example
+ * const button1 = document.body.querySelector('.class1');
+ * const button2 = document.body.querySelector('.class2');
+ * const showEvent = event => {
+ * 		console.log(event);
+ * };
+ *
+ * addListeners('click', showEvent, button1, button2);
+ */
+export const addListeners = (event, fn, ...elements) => elements.forEach(element => element.addEventListener(event, fn));
+
+/**
+ * This function removes listener for event on the HTML element or elements.
+ * @param {string} event - Type of event
+ * @param {function} fn - Method which handle event (callback)
+ * @param {array<HTMLElement>} elements - Array of HTML elements
+ * @example
+ * const button1 = document.body.querySelector('.class1');
+ * const button2 = document.body.querySelector('.class2');
+ * const showEvent = event => {
+ * 		console.log(event);
+ * };
+ *
+ * addListener('click', showEvent, button1, button2);
+ * removeListeners('click', showEvent, button1, button2);
+ */
+export const removeListeners = (event, fn, ...elements) => elements.forEach(element => element.removeEventListener(event, fn));
+
+/**
+ * This function invokes custom event.
+ * @param {string} type - Name of invoked event.
+ * @param {object} data - Data which will be sended to an event handler.
+ * @return {boolean|Error}
+ * @example
+ * addListener('showSomething', e => console.log(e.detail.message), document);
+ * dispatchCustomEvent('showSomething', {message: 'Hello!'});
+ */
+export const dispatchCustomEvent = (type, data = {}) => {
+	try {
+		window.dispatchEvent(new CustomEvent(type, {detail: data}));
+		return true;
+	} catch (error) {
+		return false;
+	}
+};
+
+/**
+ * This function converts NodeList into an array.
+ * @param {object} array - Nodelist which we want to convert into an array
+ * @return {array}
+ * @example
+ * const elements = document.querySelectorAll('p');
+ * const elementsInArray = nodeListToArray(elements);
+ */
+export const nodeListToArray = (array) => Array.prototype.slice.call(array);
+
+/**
+ * This function removes class from the HTML element.
+ * @param {HTMLElement} element - HTML element.
+ * @param {string} className - Class which will be delete.
+ * @example
+ * const paragraph = document.querySelector('p.paragraph--bold');
+ *
+ * removeClass(paragraph, 'paragraph--bold');
+ */
+export const removeClass = (element, className) => {
+	/** @type {Array} */
+	const classes = element.className.split(' ');
+	/** @type {String} */
+	let result = '';
+
+	result = classes.reduce((resultClassName, cls, i) => {
+		if (cls !== className) {
+			return resultClassName += i === classes.length - 1 ? cls : `${cls} `;
+		}
+		return resultClassName;
+	}, '');
+
+	element.className = result === '' ? element.className : result;
+};
+
+/**
+ * This function adds class to an HTML element.
+ * @param {HTMLElement} element - HTML element.
+ * @param {string} className - Class which will be delete.
+ * @example
+ * const paragraph = document.querySelector('p');
+ *
+ * addClass(paragraph, 'paragraph--bold');
+ */
+export const addClass = (element, className) => {
+	/** @type {Array} */
+	const classes = element.className.split(' ');
+
+	element.className += classes.find(cls => cls === className) ? '' : ` ${className}`;
+};
---
+++ src/__tests__/Tools.test.js
@@ -0,0 +1,239 @@
+import * as Tools from '../Tools';
+
+describe('Test of appendChilds.', () => {
+	test('Function appendChilds should append 1 child.', () => {
+		const parent = document.body;
+		const div = document.createElement('div');
+
+		div.id = 'child';
+		Tools.appendChilds(parent, div);
+
+		const child = document.querySelector('body #child');
+
+		expect(!!child).toBeTruthy();
+		expect(child.parentNode === parent).toBeTruthy();
+	});
+
+	test('Function appendChilds should append 2 childern.', () => {
+		const parent = document.body;
+		const div1 = document.createElement('div');
+		const div2 = document.createElement('div');
+
+		div1.id = 'child1';
+		div2.id = 'child2';
+		Tools.appendChilds(parent, div1, div2);
+
+		const child1 = document.querySelector('body #child1');
+		const child2 = document.querySelector('body #child2');
+
+		expect(!!child1).toBeTruthy();
+		expect(!!child2).toBeTruthy();
+		expect(child1.parentNode === parent).toBeTruthy();
+		expect(child2.parentNode === parent).toBeTruthy();
+	});
+});
+
+describe('Test of createWholeElement.', () => {
+	test('Function createWholeElement should create div element with id: "test" and class "testClass".', () => {
+		const element = Tools.createWholeElement('div', {
+			id: 'test',
+			class: ['testClass']
+		});
+
+		expect(element.nodeName).toBe('DIV');
+		expect(element.id).toBe('test');
+		expect(element.className).toBe('testClass');
+	});
+
+	test('Function createWholeElement should create div element with className: "testClass class superClass".', () => {
+		const element = Tools.createWholeElement('div', {
+			class: ['testClass', 'class', 'superClass']
+		});
+
+		expect(element.nodeName).toBe('DIV');
+		expect(element.className).toBe('testClass class superClass');
+	});
+
+	test('Function createWholeElement should create div element with data-id: "test".', () => {
+		const element = Tools.createWholeElement('div', {
+			'innerHTML': 'test'
+		});
+
+		expect(element.innerHTML).toBe('test');
+	});
+
+	test('Function createWholeElement should create div element with data-id: "test".', () => {
+		const element = Tools.createWholeElement('div', {
+			'innerText': 'test'
+		});
+
+		expect(element.innerText).toBe('test');
+	});
+
+	test('Function createWholeElement should create div element with data-id: "test".', () => {
+		const element = Tools.createWholeElement('div', {
+			'data-id': 'test'
+		});
+
+		expect(element.getAttribute('data-id')).toBe('test');
+	});
+});
+
+describe('Test of addListeners.', () => {
+	test('Function addListeners should create onClick listener on div#test.', () => {
+		const div = document.createElement('div');
+		const eventCallback = jest.fn();
+
+		div.id = 'test';
+		Tools.addListeners('click', eventCallback, div);
+
+		div.click();
+		expect(eventCallback).toHaveBeenCalled();
+	});
+
+	test('Function addListeners should create onClick listener on div#test and div#test2.', () => {
+		const div1 = document.createElement('div');
+		const div2 = document.createElement('div');
+		const eventCallback = jest.fn();
+
+		div1.id = 'test';
+		div2.id = 'test2';
+		Tools.addListeners('click', eventCallback, div1, div2);
+
+		div1.click();
+		div2.click();
+		div2.click();
+		expect(eventCallback).toHaveBeenCalledTimes(3);
+	});
+});
+
+describe('Test of removeListeners.', () => {
+	test('Function removeListeners should delete onClick listener which is on div.', () => {
+		const div = document.createElement('div');
+		const eventCallback = jest.fn();
+
+		div.addEventListener('click', eventCallback);
+		div.click();
+		expect(eventCallback).toHaveBeenCalledTimes(1);
+		Tools.removeListeners('click', eventCallback, div);
+		div.click();
+		expect(eventCallback).toHaveBeenCalledTimes(1);
+	});
+
+	test('Function removeListeners should delete onClick listener which is on div1 and div2.', () => {
+		const div1 = document.createElement('div');
+		const div2 = document.createElement('div');
+		const eventCallback = jest.fn();
+
+		div1.addEventListener('click', eventCallback);
+		div2.addEventListener('click', eventCallback);
+		div1.click();
+		div2.click();
+		div2.click();
+		expect(eventCallback).toHaveBeenCalledTimes(3);
+		Tools.removeListeners('click', eventCallback, div1, div2);
+		div1.click();
+		div2.click();
+		expect(eventCallback).toHaveBeenCalledTimes(3);
+	});
+});
+
+describe('Test of dispatchCustomEvent.', () => {
+	test('Function dispatchCustomEvent should create and dispatch testEvent event ones.', () => {
+		const eventCallback = jest.fn();
+
+		window.addEventListener('testEvent', eventCallback);
+		Tools.dispatchCustomEvent('testEvent', {});
+
+		expect(eventCallback).toHaveBeenCalledTimes(1);
+	});
+
+	test('Function dispatchCustomEvent should create and dispatch testEvent event three times.', () => {
+		const eventCallback = jest.fn();
+
+		window.addEventListener('testEvent', eventCallback);
+		Tools.dispatchCustomEvent('testEvent', {});
+		Tools.dispatchCustomEvent('testEvent', {});
+		Tools.dispatchCustomEvent('testEvent', {});
+
+		expect(eventCallback).toHaveBeenCalledTimes(3);
+	});
+
+	test('Function dispatchCustomEvent should create and dispatch testEvent event ones with data: {message: "test"}.', () => {
+		const eventCallback = jest.fn(event => event.detail.message);
+
+		window.addEventListener('testEvent', eventCallback);
+		Tools.dispatchCustomEvent('testEvent', {message: 'test'});
+
+		expect(eventCallback).toHaveBeenCalledTimes(1);
+		expect(eventCallback).toHaveReturnedWith('test');
+	});
+
+	test('Function dispatchCustomEvent should return true after succeessfully dispatch.', () => {
+		const oldWindowCustomEvent = window.CustomEvent;
+
+		window.CustomEvent = {};
+		expect(Tools.dispatchCustomEvent('testEvent', {message: 'test'})).toBeFalsy();
+		window.CustomEvent = oldWindowCustomEvent;
+	});
+
+	test('Function dispatchCustomEvent should return false after unsuccessfully dispatch.', () => {
+		expect(Tools.dispatchCustomEvent('testEvent', {message: 'test'})).toBeTruthy();
+	});
+});
+
+describe('Test of nodeListToArray.', () => {
+	test('Function nodeListToArray should convert NodeList from querySelector into array.', () => {
+		const div = document.createElement('div');
+
+		div.className = 'origDiv';
+		document.body.appendChild(div);
+
+		const divs = document.querySelectorAll('div.origDiv');
+		const nodeArray = Tools.nodeListToArray(divs);
+
+
+		expect(Array.isArray(nodeArray)).toBe(true);
+		expect(nodeArray.pop()).toEqual(div);
+	});
+});
+
+describe('Test of removeClass.', () => {
+	test('Function removeClass should remove class: "test1" from "div.test1.test2.test3.', () => {
+		const div = document.createElement('div');
+
+		div.className = 'test1 test2 test3';
+		Tools.removeClass(div, 'test1');
+
+		expect(div.className).toBe('test2 test3');
+	});
+
+	test('Function removeClass should remove class: "test5" from "div.test1.test2.test3.', () => {
+		const div = document.createElement('div');
+
+		div.className = 'test1 test2 test3';
+		Tools.removeClass(div, 'test5');
+
+		expect(div.className).toBe('test1 test2 test3');
+	});
+});
+
+describe('Test of addClass.', () => {
+	test('Function addClass should add class: "test1" from "div.test2.test3.', () => {
+		const div = document.createElement('div');
+
+		div.className = 'test2 test3';
+		Tools.addClass(div, 'test1');
+
+		expect(div.className).toBe('test2 test3 test1');
+	});
+
+	test('Function addClass should add class: "test1" from "div.test1.test2.test3.', () => {
+		const div = document.createElement('div');
+
+		div.className = 'test1 test2 test3';
+		Tools.addClass(div, 'test1');
+
+		expect(div.className).toBe('test1 test2 test3');
+	});
+});
